079960e5f005affad8dcf5b5fe69b855de04e0f4
olkc <colk@vt.edu>
2014-06-21 00:35:28 -0500

    login
    The log in GUI is DONE
    (yay copy and paste)

--- PublicSafetyHack/gen/com/att/publicsafetyhack/R.java
+++ PublicSafetyHack/gen/com/att/publicsafetyhack/R.java
@@ -8,7 +8,19 @@
 package com.att.publicsafetyhack;
 
 public final class R {
+    public static final class array {
+        public static final int buildingArrayDallas=0x7f060004;
+        public static final int cityArrayTexas=0x7f060003;
+        public static final int countryArray=0x7f060001;
+        public static final int dateArray=0x7f060006;
+        public static final int drawer_array=0x7f060000;
+        public static final int floorArray2=0x7f060005;
+        public static final int stateArrayUSA=0x7f060002;
+    }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int hintcolor=0x7f080000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -25,32 +37,65 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f0a0009;
+        public static final int container=0x7f0a0008;
+        public static final int imageView1=0x7f0a0003;
+        public static final int login=0x7f0a0006;
+        public static final int login_button=0x7f0a0007;
+        public static final int login_form=0x7f0a0002;
+        public static final int login_status=0x7f0a0000;
+        public static final int login_status_message=0x7f0a0001;
+        public static final int prompt_password=0x7f0a0005;
+        public static final int prompt_username=0x7f0a0004;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_main=0x7f030002;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_forgot_password=0x7f05001c;
+        public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_register=0x7f05001a;
+        public static final int action_sign_in_short=0x7f05001b;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int building_hint=0x7f05000d;
+        public static final int building_prompt=0x7f050012;
+        public static final int city_hint=0x7f05000c;
+        public static final int city_prompt=0x7f050011;
+        public static final int countryPrompt=0x7f050016;
+        public static final int country_hint=0x7f05000a;
+        public static final int country_prompt=0x7f05000f;
+        public static final int drawer_close=0x7f050015;
+        public static final int drawer_open=0x7f050014;
+        public static final int error_field_required=0x7f050021;
+        public static final int error_incorrect_password=0x7f050020;
+        public static final int error_invalid_email=0x7f05001e;
+        public static final int error_invalid_password=0x7f05001f;
+        public static final int floor_hint=0x7f05000e;
+        public static final int floor_prompt=0x7f050013;
+        public static final int hello_world=0x7f050009;
+        public static final int login=0x7f050004;
+        public static final int login_home=0x7f050002;
+        public static final int login_image=0x7f050005;
+        public static final int login_progress_signing_in=0x7f05001d;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050018;
+        public static final int prompt_password=0x7f050019;
+        public static final int prompt_username=0x7f050003;
+        public static final int state_hint=0x7f05000b;
+        public static final int state_prompt=0x7f050010;
+        public static final int title_activity_drawer=0x7f050007;
+        public static final int title_activity_login=0x7f050017;
+        public static final int title_activity_main=0x7f050006;
+        public static final int title_activity_select_location=0x7f050008;
     }
     public static final class style {
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -62,10 +107,11 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070002;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070000;
+        public static final int LoginFormContainer=0x7f070001;
     }
 }
