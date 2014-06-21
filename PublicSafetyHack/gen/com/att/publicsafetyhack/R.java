11cba4a2cb999dbbb2b08ae9b61023a864af0ed9
EmVilleda <emvilledaprincipe@yahoo.com>
2014-06-21 05:18:11 -0500

    Controller Strings
    Changes text into strings

--- PublicSafetyHack/gen/com/att/publicsafetyhack/R.java
+++ PublicSafetyHack/gen/com/att/publicsafetyhack/R.java
@@ -31,7 +31,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0012;
+        public static final int action_settings=0x7f0a0016;
         public static final int button1=0x7f0a0011;
         public static final int container=0x7f0a0008;
         public static final int editText1=0x7f0a000b;
@@ -47,42 +47,52 @@ public final class R {
         public static final int radio1=0x7f0a000f;
         public static final int radio2=0x7f0a0010;
         public static final int radioGroup1=0x7f0a000d;
+        public static final int textTimer=0x7f0a0014;
         public static final int textView1=0x7f0a0009;
         public static final int textView2=0x7f0a000a;
         public static final int textView3=0x7f0a000c;
+        public static final int textView4=0x7f0a0013;
+        public static final int toggle1=0x7f0a0012;
+        public static final int toggleButton1=0x7f0a0015;
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int fragment_main=0x7f030002;
+        public static final int missioncontrolscreen=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_forgot_password=0x7f050010;
+        public static final int action_forgot_password=0x7f050015;
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f05000e;
-        public static final int action_sign_in_short=0x7f05000f;
+        public static final int action_sign_in_register=0x7f050013;
+        public static final int action_sign_in_short=0x7f050014;
         public static final int address=0x7f050006;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f050015;
-        public static final int error_incorrect_password=0x7f050014;
-        public static final int error_invalid_email=0x7f050012;
-        public static final int error_invalid_password=0x7f050013;
+        public static final int error_field_required=0x7f05001a;
+        public static final int error_incorrect_password=0x7f050019;
+        public static final int error_invalid_email=0x7f050017;
+        public static final int error_invalid_password=0x7f050018;
+        public static final int horiz_line=0x7f05000e;
+        public static final int in_out=0x7f05000d;
         public static final int launch_mission=0x7f050005;
         public static final int login=0x7f050008;
         public static final int login_home=0x7f050002;
         public static final int login_image=0x7f050009;
-        public static final int login_progress_signing_in=0x7f050011;
+        public static final int login_progress_signing_in=0x7f050016;
+        public static final int member=0x7f05000b;
         public static final int mission=0x7f050004;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f05000c;
-        public static final int prompt_password=0x7f05000d;
+        public static final int prompt_email=0x7f050011;
+        public static final int prompt_password=0x7f050012;
         public static final int prompt_username=0x7f050003;
         public static final int team_members=0x7f050007;
-        public static final int title_activity_login=0x7f05000b;
+        public static final int temp_time=0x7f05000f;
+        public static final int time_elapsed=0x7f05000c;
+        public static final int title_activity_login=0x7f050010;
         public static final int title_activity_main=0x7f05000a;
     }
     public static final class style {
